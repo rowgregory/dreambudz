94b4d17b8ca2c84319c8bda53f7e4f1354c0b021
rowgregory <rowgregory@gmail.com>
2024-07-17 16:39:11 -0400

    Add private route component

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -5,25 +5,28 @@ import AdminSideNavigation from '../redux/features/dashboard/components/AdminSid
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import AdminMobileNavigation from '../redux/features/dashboard/components/AdminMobileNavigation';
+import PrivateRoute from '../components/common/PrivateRoute';
 
 const AdminLayout = ({ children }: { children: ReactNode }) => {
   const [toggleMobileMenu, setToggleMobileMenu] = useState(false);
   const close = () => setToggleMobileMenu(false);
   return (
-    <div className="flex min-h-screen">
-      <FontAwesomeIcon onClick={() => setToggleMobileMenu(!toggleMobileMenu)}
-        icon={faBars}
-        className="block lg:hidden fa-solid text-lime-500 col-span-2 py-2 pl-2 pr-3 w-fit duration-200 hover:text-lime-400 absolute top-2 right-2 cursor-pointer z-20"
-      />
-      <AdminMobileNavigation
-        toggleMobileMenu={toggleMobileMenu}
-        close={close}
-      />
-      <aside className="hidden lg:block lg:w-[240px] bg-zinc-900">
-        <AdminSideNavigation />
-      </aside>
-      <main className="max-w-screen-xl w-full mx-auto">{children}</main>
-    </div>
+    <PrivateRoute>
+      <div className="flex min-h-screen">
+        <FontAwesomeIcon onClick={() => setToggleMobileMenu(!toggleMobileMenu)}
+          icon={faBars}
+          className="block lg:hidden fa-solid text-lime-500 col-span-2 py-2 pl-2 pr-3 w-fit duration-200 hover:text-lime-400 absolute top-2 right-2 cursor-pointer z-20"
+        />
+        <AdminMobileNavigation
+          toggleMobileMenu={toggleMobileMenu}
+          close={close}
+        />
+        <aside className="hidden lg:block lg:w-[240px] bg-zinc-900">
+          <AdminSideNavigation />
+        </aside>
+        <main className="max-w-screen-xl w-full mx-auto">{children}</main>
+      </div>
+    </PrivateRoute>
   );
 };
 
--- app/components/common/PrivateRoute.tsx
+++ app/components/common/PrivateRoute.tsx
@@ -1,33 +1,37 @@
-import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import Spinner from './Spinner';
+import { RootState, useAppSelector } from '@/app/redux/store';
 
 interface PrivateRouteProps {
   children: React.ReactNode;
 }
 
 const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
-  const { data: session, status } = useSession();
-  const router = useRouter();
-  const [loading, setLoading] = useState(true);
+  const auth = useAppSelector((state: RootState) => state.auth);
+  const token = auth.token;
+  const isAdmin = auth.isAdmin;
+  const navigate = useRouter();
+  let [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // Redirect to login page if user is not logged in
-    if (status !== 'loading' && !session) {
-      router.push('/auth/login');
+    if (!token && !isAdmin) {
+      navigate.push('/auth/login');
     }
 
-    // Set loading to false once the authentication status is determined
     setLoading(false);
-  }, [status, session, router]);
 
-  // Show a loading state while checking authentication
-  if (loading || status === 'loading') {
-    return <Spinner />;
+  }, [token, navigate, isAdmin]);
+
+  if (loading) {
+    return (
+      <div className="min-h-screen w-full bg-zinc-950 flex justify-center pt-36">
+        <Spinner fill="fill-white" wAndH='w-10 h-10' />
+      </div>
+    );
   }
 
-  // Render the protected content if user is logged in
-  return session ? children : null;
+  return token ? children : null;
 };
-export default PrivateRoute
+
+export default PrivateRoute;
